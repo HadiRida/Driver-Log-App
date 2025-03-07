422b1bea88a88a94d5c98dc88636f61ef8a65d2b
Hadi Rida <120185538+HadiRida@users.noreply.github.com>
2025-03-07 16:05:30 +0200

    create

--- driver_logs/driver_logs_backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import TripDeleteView, TripListCreateView, TripDetailView, LogEntryListCreateView
-
-urlpatterns = [
-    path('trips/', TripListCreateView.as_view(), name='trip-list-create'),
-    path('trips/<int:pk>/', TripDetailView.as_view(), name='trip-detail'),
-    path('trips/<int:pk>/delete/', TripDeleteView.as_view(), name='trip-delete'),  # Add this line
-    path('log-entries/', LogEntryListCreateView.as_view(), name='log-entry-list-create'),
-]
